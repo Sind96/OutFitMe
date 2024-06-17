ae38186b5eeb75d45ffc64647b6acdc20fc77c3c
Sind96 <sindhujany@gmail.com>
2024-06-17 19:36:30 +0100

    Route completed for Sign up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,107 +1,24 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import Sidebar from './Components/Sidebar/Sidebar';
-import DisplayContainer from './Components/DisplayContainer/DisplayContainer';
-import { getWeatherData } from './Services/apiService';
-import LoginPage from './Components/LoginPage/LoginPage';
-import Gallery from './Components/Gallery/Gallery';
-import { IWeatherDisplayProps } from './Types/App.Types'
+// import Sidebar from './Components/Sidebar/Sidebar';
+// import DisplayContainer from './Components/DisplayContainer/DisplayContainer';
+// import LoginPage from './Components/LoginPage/LoginPage';
+// import Gallery from './Components/Gallery/Gallery';
+import SignIn from './Pages/Login/signIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   //TODO: Style the page where user accepts to give their location first, have that accept button get weather and random outfit
   // to avoid having to click two buttons
 
-  //NAME
-  const [name, setName] = useState<string>('');
-
-  //WEATHER
-  const [weatherData, setWeatherData] = useState<IWeatherDisplayProps>({
-    location: '',
-    temp: '',
-    temp_max: '',
-    temp_min: '',
-    humidity: '',
-    feels_like: '',
-    description: '',
-  });
-
-  const [emoji, setEmoji] = useState<string>('');
-  const [clicked, setClicked] = useState<boolean>(false);
-
+  
   //GALLERIES
   const [itemType, setItemType] = useState<string>('');
   const [gallery, setGallery] = useState<string>('');
 
   ////////////////////////////////////////////////////////////////////////////
-
-  //NAME
-  const handleName = (event : React.MouseEvent) => {
-    setName(event.currentTarget.value);
-  };
-
-  //WEATHER
-  useEffect(() => {
-    if (weatherData.description === '') return;
-
-    const descriptionToday = weatherData.description;
-
-    switch (descriptionToday) {
-      case 'Thunderstorm':
-        setEmoji('⛈');
-        break;
-      case 'Drizzle':
-        setEmoji('🌧');
-        break;
-      case 'Rain':
-        setEmoji('🌧');
-        break;
-      case 'Snow':
-        setEmoji('🌨');
-        break;
-      case 'Clouds':
-        setEmoji('⛅');
-        break;
-      default: //'Clear'
-        setEmoji('☀');
-    }
-  }, [weatherData.description]);
-
-  const getLocation = (event : React.MouseEvent) => {
-    event.preventDefault();
-
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-
-        getWeather(lat, lon);
-        setClicked(true);
-      });
-    } else {
-      alert('Please enable geolocation to use this app.'); //TODO: maybe try sweetalert2?  https://sweetalert2.github.io/
-    }
-  };
-
-  const getWeather = (lat: number, lon: number) => {
-    //apiService method for weather gets lat and lon as arguments to add to the url
-    getWeatherData(lat, lon).then((weatherData) => {
-      const {
-        name: location,
-        main: { temp, humidity, feels_like, temp_max, temp_min },
-        weather: [{ main }],
-      } = weatherData;
-      setWeatherData({
-        location: location,
-        temp: temp,
-        temp_max: temp_max,
-        temp_min: temp_min,
-        humidity: humidity,
-        feels_like: feels_like,
-        description: main,
-      });
-    });
-  };
 
   //GALLERIES
   const onMenuClick = async (itemType: string) => {
@@ -111,7 +28,12 @@ function App() {
 
   return (
     <>
-       {!clicked ? (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<SignIn />}  />
+    </Routes>
+    </BrowserRouter>
+       {/* {!clicked ? (
         <LoginPage
           getLocation={getLocation}
           name={name}
@@ -135,7 +57,7 @@ function App() {
             <Sidebar onMenuClick={onMenuClick} />
           </div>
          </div>
-      )}
+      )} */}
      </>
   );
 }
--- client/src/Pages/Login/signIn.tsx
+++ client/src/Pages/Login/signIn.tsx
@@ -1,19 +1,151 @@
 import styles from "./login.module.css"
+import { useState, useEffect } from 'react';
+import { getWeatherData } from '../../Services/apiService';
+import { IWeatherDisplayProps } from '../../Types/App.Types'
+import { Link } from 'react-router-dom';
 
 export default function SignIn () {
+  
+  const [signInForm, setSignInForm] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setSignInForm({
+      ...signInForm,
+      [e.target.name]: e.target.value
+    })
+  }
+  
+    //WEATHER
+
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  const getLocation = (event : React.MouseEvent) => {
+    event.preventDefault();
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        getWeather(lat, lon);
+        setClicked(true);
+      });
+    } else {
+      alert('Please enable geolocation to use this app.'); //TODO: maybe try sweetalert2?  https://sweetalert2.github.io/
+    }
+  };
+
+
+  const getWeather = (lat: number, lon: number) => {
+    //apiService method for weather gets lat and lon as arguments to add to the url
+    getWeatherData(lat, lon).then((weatherData) => {
+      const {
+        name: location,
+        main: { temp, humidity, feels_like, temp_max, temp_min },
+        weather: [{ main }],
+      } = weatherData;
+      setWeatherData({
+        location: location,
+        temp: temp,
+        temp_max: temp_max,
+        temp_min: temp_min,
+        humidity: humidity,
+        feels_like: feels_like,
+        description: main,
+      });
+    });
+  };
+
+  const [weatherData, setWeatherData] = useState<IWeatherDisplayProps>({
+    location: '',
+    temp: '',
+    temp_max: '',
+    temp_min: '',
+    humidity: '',
+    feels_like: '',
+    description: '',
+  });
+
+  const [emoji, setEmoji] = useState<string>('');
+
+    //WEATHER
+    useEffect(() => {
+      if (weatherData.description === '') return;
+  
+      const descriptionToday = weatherData.description;
+  
+      switch (descriptionToday) {
+        case 'Thunderstorm':
+          setEmoji('⛈');
+          break;
+        case 'Drizzle':
+          setEmoji('🌧');
+          break;
+        case 'Rain':
+          setEmoji('🌧');
+          break;
+        case 'Snow':
+          setEmoji('🌨');
+          break;
+        case 'Clouds':
+          setEmoji('⛅');
+          break;
+        default: //'Clear'
+          setEmoji('☀');
+      }
+    }, [weatherData.description]);
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+    }
+
+    const [ isLoading, setIsLoading] = useState(false);
+    const [ error, setError] = useState(false);
+
 
   return (
-  <div className={styles.SignInContainer}>
-
-<form className={styles.flexForm}>
-        <input type="text" placeholder="email@domain.com"/> 
-        <input type="password" placeholder="Password"/>
-        <button type="submit">Login</button>
+  <main className="bg-blue-50 h-screen">
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl text-center font-semibold my-7"> Sign In </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 "
+        >
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={signInForm.username}
+          className="bg-white p-3 rounded-lg"
+          onChange={handleChange}
+          required
+          />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={signInForm.password}
+          className="bg-white p-3 rounded-lg"
+          onChange={handleChange}
+          required
+          />
+        <button
+          disabled={isLoading}
+          className="bg-blue-600 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-60">
+            {isLoading ? 'Creating' : 'SIGN IN'}
+        </button>
       </form>
-
-      <div className={styles.SignInExtra}>
-        <p>First time?<span className={styles.redFont} > Sign up now!</span></p>
+      <div className="flex gap-2 mt-5">
+        <p>{`Don't have an account?`}</p>
+        <Link to={'/sign-up'}>
+          <span className="text-blue-500">Sign up</span>
+        </Link>
       </div>
-      </div>
+      <p className="text-red-700 mt-5"> {error ? error  || 'something went wrong' : ''} </p>
+    </div>
+  </main>
   );
 }
