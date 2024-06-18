be2dee5ec401af562959f7dd0af7bac981900935
Sulo Siigur <sulo.siigur@gmail.com>
2024-06-18 17:13:38 +0100

    added: backend route testing

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,10 +1,26 @@
-const Koa = require('koa');
-const app = require('../index'); //Double check if this is correct
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
-const { PORT } = require('./utils/config');
-const router = require('./routes/images.route');
-
+const app = require('../index.js');
 const Supertest = require('supertest');
-const request = require(app);
+const request = Supertest(app.listen());
+
+describe('Index Routes', () => {
+    it('GET /test', async () => {
+        const response = await request.get('/test');
+        expect(response.status).toEqual(200);
+    })
+
+    it('GET /getAllItems', async () => {
+        const response = await request.get('/getAllItems');
+        expect(response.status).toEqual(200);
+    })
+    
+    it('GET /getRandomItem', async () => {
+        const response = await request.get('/getRandomItem');
+        expect(response.status).toEqual(200);
+    })
+
+    // it('POST /upload', async () => {
+    //     const response = await request.post('/upload');
+    //     expect(response.status).toEqual(200);
+    // })
+})
 
