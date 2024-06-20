772b8975f88745a0babd24742af0def74843d252
Sind96 <sindhujany@gmail.com>
2024-06-20 12:30:07 +0100

    changing names of files

--- client/src/test/SignIn.test.tsx
+++ client/src/test/SignIn.test.tsx
@@ -2,7 +2,7 @@ import { render as rtlRender, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 
-import SignIn from "../pages/login/SignIn";
+import SignIn from "../Pages/Login/signIn2";
 import { store } from "../store/store";
 import { ReactNode } from "react";
 import { JSX } from "react/jsx-runtime";
