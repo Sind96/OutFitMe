27de1cfebf90f8a6f9555c70d6816c3a20f920b3
Sind96 <sindhujany@gmail.com>
2026-06-16 18:35:07 +0100

    chore: improve local development configuration

--- server/dbconnect.js
+++ server/dbconnect.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { DB_NAME, DB_PORT, TEST_DB_NAME, NODE_ENV } = process.env;
 
-if(NODE_ENV === 'test') {
+if (NODE_ENV === "test") {
   mongoose
-  .connect(`mongodb://127.0.0.1:${DB_PORT}/${TEST_DB_NAME}`, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log(`🎃 Succesfully connected to ${TEST_DB_NAME} on port ${DB_PORT}!`);
-  })
-  .catch((error) => {
-    console.log(`Something went wrong! ${error}`);
-  });
-
+    .connect(`mongodb://127.0.0.1:${DB_PORT}/${TEST_DB_NAME}`, {})
+    .then(() => {
+      console.log(
+        `🎃 Succesfully connected to ${TEST_DB_NAME} on port ${DB_PORT}!`,
+      );
+    })
+    .catch((error) => {
+      console.log(`Something went wrong! ${error}`);
+    });
 } else {
   mongoose
-  .connect(`mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}`, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log(`🎃 Succesfully connected to ${DB_NAME} on port ${DB_PORT}!`);
-  })
-  .catch((error) => {
-    console.log(`Something went wrong! ${error}`);
-  });
-
+    .connect(`mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}`, {})
+    .then(() => {
+      console.log(`🎃 Succesfully connected to ${DB_NAME} on port ${DB_PORT}!`);
+    })
+    .catch((error) => {
+      console.log(`Something went wrong! ${error}`);
+    });
 }
 module.exports = mongoose;
