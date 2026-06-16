fc060092f8538bba13066cc436f48ec4bf834f29
Sind96 <sindhujany@gmail.com>
2026-06-16 13:12:01 +0100

    refactor: migrate routes from koa to express

--- server/app.js
+++ server/app.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const authRouter = require("./routes/auth.route");
+const imageRouter = require("./routes/images.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'OutFitMe API is running' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "OutFitMe API is running" });
 });
 
+// Routes
+app.use(authRouter);
+app.use(imageRouter);
+
+// 404 handler LAST
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
 
 module.exports = app;
--- server/routes/images.route.js
+++ server/routes/images.route.js
@@ -1,20 +1,15 @@
-'use strict';
+const express = require("express");
+const router = express.Router();
 
-const Router = require('@koa/router');
-const router = new Router();
-const image = require('../controllers/image.controllers');
+const image = require("../controllers/image.controllers");
 
-//MVP routes
-router.post('/upload', image.postImage);
-router.get('/getRandomItem/:item/:tempToday/:rainToday', image.getRandomItem);
-router.get('/getAllItems/:item', image.getAllItems);
-router.get('/test', async (ctx) => {
-    try {
-        ctx.status = 200;
-    } catch (err) {
-        ctx.status = 500;
-        console.log(err);
-    }
-})
+// MVP routes
+router.post("/upload", image.postImage);
+router.get("/getRandomItem/:item/:tempToday/:rainToday", image.getRandomItem);
+router.get("/getAllItems/:item", image.getAllItems);
+
+router.get("/test", (req, res) => {
+  res.sendStatus(200);
+});
 
 module.exports = router;
