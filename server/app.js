4cb190fe50ef211b25521d4a3aa5f87fd3bbbfce
Sind96 <sindhujany@gmail.com>
2026-06-20 00:38:50 +0100

    feat: add global error handling utilities

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 
 const authRouter = require("./routes/auth.routes");
 const clothingItemRouter = require("./routes/clothingItem.routes");
+const errorHandler = require("./middleware/error.middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "OutFitMe API is running" });
