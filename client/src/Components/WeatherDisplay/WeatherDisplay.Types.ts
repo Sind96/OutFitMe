2ff57e055cae938bf4ca9b36cc6ed20e190bfd30
Sind96 <sindhujany@gmail.com>
2026-06-21 00:28:37 +0100

    refactor: clean up weather display component

---
+++ client/src/Components/WeatherDisplay/WeatherDisplay.Types.ts
@@ -0,0 +1,6 @@
+import type { IWeatherDisplayProps } from "../../Types/App.Types";
+
+export interface WeatherDisplayProps {
+  weatherData: IWeatherDisplayProps;
+  emoji: string;
+}
--- client/src/Components/WeatherDisplay/WeatherDisplay.tsx
+++ client/src/Components/WeatherDisplay/WeatherDisplay.tsx
@@ -1,41 +1,36 @@
-import './WeatherDisplay.css';
+import "./WeatherDisplay.css";
 import { FaLocationDot } from "react-icons/fa6";
-
-interface WeatherDisplayProps {
-  weatherData: {
-    location: string;
-    temp: string;
-    temp_min: string;
-    temp_max: string;
-    humidity: string;
-    feels_like: string;
-    description: string;
-  };
-  emoji: string;
-
-}
+import { WeatherDisplayProps } from "./WeatherDisplay.Types";
 
 function WeatherDisplay({ weatherData, emoji }: WeatherDisplayProps) {
-
   return (
     <div className="weather-container">
-
       <div className="top-data">
-        <p className="locationapidata"><FaLocationDot className='locationicon'/>{weatherData.location}</p>
+        <p className="locationapidata">
+          <FaLocationDot className="locationicon" />
+          {weatherData.location}
+        </p>
       </div>
 
-
-      <div className='tempandemojicontainer'>
-        <div className='tempandemoji'>
-        <p className="temperatureapidata">{Math.round(Number(weatherData.temp))}ºC</p><div className="emojiapidata">{emoji}</div>
+      <div className="tempandemojicontainer">
+        <div className="tempandemoji">
+          <p className="temperatureapidata">
+            {Math.round(Number(weatherData.temp))}ºC
+          </p>
+          <div className="emojiapidata">{emoji}</div>
         </div>
-        <div className='humidityFeelslikeDesc'>
-        <p className="apidata">{Math.round(Number(weatherData.temp_min))}º/{Math.round(Number(weatherData.temp_max))}º</p>
-        <p className="humidity">Humidity: {weatherData.humidity}% </p>
-        <p className="feelslike">Feels like: {Math.round(Number(weatherData.feels_like))}ºC</p>
-        <p className="weatherDescription">{weatherData.description}</p>
+        <div className="humidityFeelslikeDesc">
+          <p className="apidata">
+            {Math.round(Number(weatherData.temp_min))}º/
+            {Math.round(Number(weatherData.temp_max))}º
+          </p>
+          <p className="humidity">Humidity: {weatherData.humidity}% </p>
+          <p className="feelslike">
+            Feels like: {Math.round(Number(weatherData.feels_like))}ºC
+          </p>
+          <p className="weatherDescription">{weatherData.description}</p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
