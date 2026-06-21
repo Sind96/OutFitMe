3de353e244c41c44a684ca8fbaaa14b7265f3a43
Sind96 <sindhujany@gmail.com>
2026-06-21 02:08:05 +0100

    refactor: clean up private route component

--- client/src/Components/PrivateRoute/PrivateRoute.tsx
+++ client/src/Components/PrivateRoute/PrivateRoute.tsx
@@ -4,5 +4,5 @@ import { Outlet, Navigate } from "react-router-dom";
 export default function PrivateRoute() {
   const { currentUser } = useAppSelector((state) => state.user);
 
-  return currentUser ? <Outlet /> : <Navigate to={"/"} />;
+  return currentUser ? <Outlet /> : <Navigate to="/" replace />;
 }
