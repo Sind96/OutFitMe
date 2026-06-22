3b8f6af113094660ad551fd29266298fffa8ee32
Sind96 <sindhujany@gmail.com>
2026-06-22 20:32:28 +0100

    fix: improve sidebar menu behaviour

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { CiLogout } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
 import { logOut } from "../../services/authService";
 import { useAppDispatch } from "../../store/hooks/reduxHooks";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "../../store/slices/userSlice";
 import { GiClothes } from "react-icons/gi";
 import { useState } from "react";
@@ -15,17 +15,24 @@ import type { SidebarProps } from "./Sidebar.types";
 function Sidebar({ onMenuClick }: SidebarProps) {
   const [showClothingMenu, setShowClothingMenu] = useState(false);
   const [showOptionsMenu, setShowOptionsMenu] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const toggleClothingMenu = () => {
+    if (!showClothingMenu) {
+      setShowOptionsMenu(false);
+    }
+
     setShowClothingMenu(!showClothingMenu);
   };
 
   const toggleOptionsMenu = () => {
+    if (!showOptionsMenu) {
+      setShowClothingMenu(false);
+    }
+
     setShowOptionsMenu(!showOptionsMenu);
   };
-
-  //TODO: Add functionality to see (1) galleries by item type (2) liked outfits
-  const dispatch = useAppDispatch();
 
   const handleSignOut = async () => {
     try {
@@ -39,54 +46,70 @@ function Sidebar({ onMenuClick }: SidebarProps) {
     }
   };
 
+  const handleGalleryClick = (itemType: string) => {
+    onMenuClick(itemType);
+    setShowClothingMenu(false);
+    navigate("/home");
+  };
+
   return (
     <div className="sidebar-nav">
-      <button className="sidebar-icon" onClick={() => onMenuClick("")}>
-        <Link to={"/home"} className="sidebar-icon">
-          {" "}
-          <IoHomeOutline />{" "}
-        </Link>
-      </button>
+      <Link to="/home" className="sidebar-icon" onClick={() => onMenuClick("")}>
+        <IoHomeOutline />
+      </Link>
 
       <button className="sidebar-icon">
         <PiHeartStraight />
       </button>
 
-      {showClothingMenu && (
-        <div className="iconPopUps">
-          <button className="sidebar-icon" onClick={() => onMenuClick("top")}>
-            <PiTShirt />
-          </button>
-          <button
-            className="sidebar-icon"
-            onClick={() => onMenuClick("bottom")}
-          >
-            <PiPants />
-          </button>
-          <button className="sidebar-icon" onClick={() => onMenuClick("shoe")}>
-            <PiSneaker />
-          </button>
-        </div>
-      )}
+      <div className="menuWrapper">
+        <button className="sidebar-icon" onClick={toggleClothingMenu}>
+          <GiClothes />
+        </button>
 
-      <button className="sidebar-icon" onClick={toggleClothingMenu}>
-        <GiClothes />
-      </button>
+        {showClothingMenu && (
+          <div className="iconPopUps">
+            <button
+              className="sidebar-icon"
+              onClick={() => handleGalleryClick("top")}
+            >
+              <PiTShirt />
+            </button>
 
-      {showOptionsMenu && (
-        <div className="iconPopUps2">
-          <Link to={"/profile"} className="sidebar-icon">
-            <CgProfile />
-          </Link>
-          <button className="sidebar-icon" onClick={handleSignOut}>
-            <CiLogout />
-          </button>
-        </div>
-      )}
+            <button
+              className="sidebar-icon"
+              onClick={() => handleGalleryClick("bottom")}
+            >
+              <PiPants />
+            </button>
 
-      <button className="sidebar-icon" onClick={toggleOptionsMenu}>
-        <IoMdMore />
-      </button>
+            <button
+              className="sidebar-icon"
+              onClick={() => handleGalleryClick("shoe")}
+            >
+              <PiSneaker />
+            </button>
+          </div>
+        )}
+      </div>
+
+      <div className="menuWrapper">
+        <button className="sidebar-icon" onClick={toggleOptionsMenu}>
+          <IoMdMore />
+        </button>
+
+        {showOptionsMenu && (
+          <div className="iconPopUps2">
+            <Link to={"/profile"} className="sidebar-icon">
+              <CgProfile />
+            </Link>
+
+            <button className="sidebar-icon" onClick={handleSignOut}>
+              <CiLogout />
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
