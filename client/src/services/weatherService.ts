a185ace09c88ed023e074339445caaf07b43bb55
Sind96 <sindhujany@gmail.com>
2026-06-23 23:32:41 +0100

    fix: use backend weather endpoint

--- client/src/services/weatherService.ts
+++ client/src/services/weatherService.ts
@@ -1,17 +1,11 @@
-import { handleResponse } from "./apiClient";
+import { baseURL, handleResponse } from "./apiClient";
 import type { OpenWeatherResponse } from "../types/weather.types";
-
-const weatherAPIkey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-console.log(weatherAPIkey);
 
 const getWeatherData = async (
   lat: number,
   lon: number,
 ): Promise<OpenWeatherResponse> => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPIkey}&units=metric`;
-  console.log(url);
-
-  const response = await fetch(url);
+  const response = await fetch(`${baseURL}/api/weather?lat=${lat}&lon=${lon}`);
 
   return handleResponse<OpenWeatherResponse>(response);
 };
